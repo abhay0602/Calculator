0a400686afc06bf38953525147fe24dcbf5a3db2
Abhay Saxena <134828058+abhay0602@users.noreply.github.com>
2023-06-26 16:50:07 +0530

    file uploaded

---
+++ script.js
@@ -0,0 +1,29 @@
+"use strict"
+
+let input=document.getElementById('inputBox');
+let buttons=document.querySelectorAll('button');
+console.log(buttons)
+let string="";      // jis mein aapna result store karenge thats why we taking empty string//
+let arr=Array.from(buttons);  
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
+
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0,string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+    })
+})
+
